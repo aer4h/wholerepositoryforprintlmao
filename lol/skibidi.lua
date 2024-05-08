af0598379a5c7d84718bd75ac686b5bef4cc02ca
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-05-08 11:07:50 +0800

    Update skibidi.lua

--- lol/skibidi.lua
+++ lol/skibidi.lua
@@ -1,1 +1,1 @@
-print("g6 is smd lil nigga") print("lame ass mods") print("goofy nigga hitbox") print("easy ass client bring") print("this shit is funny") print("fuck you mean anticheat?")
+print("g6 smd lil nigga") print("lame ass mods") print("goofy nigga hitbox") print("easy ass client bring") print("this shit is funny") print("fuck you mean anticheat?")
